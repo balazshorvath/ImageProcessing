a1fec7f5c8923575e6f608328bd99380a20dadf8
balazshorvath <balazs.peter.horvath@gmail.com>
2018-05-03 14:53:16 -0300

    added new images README finished image prediction

---
+++ README.md
@@ -0,0 +1,21 @@
+##Projects
+###Labello
+We have images with black background color and one object.
+There's a collection of images with "Labello" on them.
+
+The task is to create a training set out of the images, and train the ML algorithm to recognize, which image
+has a "Labello" on them and which images don't.
+
+I used OpenCV
+
+##Dependencies
+###OpenCV
+Only for coke_bottle and labello projects.
+```
+conda install -c conda-forge opencv
+```
+###Matplotlib
+###Scipy
+Only for convolution project.
+###PIL
+Only for convolution project.
--- labello/labello_recognition.py
+++ labello/labello_recognition.py
@@ -2,9 +2,13 @@ import numpy as np
 
 from helpers.labello_helpers import collect_image_data
 from matplotlib import pylab as pl
+from sklearn import tree
+from random import shuffle
 
 training_labello_features = np.array(collect_image_data("images\\training_labello\\*"))
 training_other_features = np.array(collect_image_data("images\\training_other\\*"))
+test_labello_features = np.array(collect_image_data("images\\test_labello\\*"))
+test_other_features = np.array(collect_image_data("images\\test_other\\*"))
 
 pl.figure(1)
 pl.subplot(2, 2, 1)
@@ -15,7 +19,50 @@ pl.subplot(2, 2, 3)
 pl.bar(range(2), [training_labello_features[:, 2].mean(), training_other_features[:, 2].mean()], color='Blue')
 pl.subplot(2, 2, 4)
 pl.bar(range(2), [training_labello_features[:, 3].mean(), training_other_features[:, 3].mean()], color='Red')
-# pl.figure(2)
-# pl.scatter(training_labello_features[:, 0], training_labello_features[:, 3])
-# pl.scatter(training_other_features[:, 0], training_other_features[:, 3])
+#pl.figure(2)
+#pl.scatter(training_labello_features[:, 0], training_labello_features[:, 3])
+#pl.scatter(training_other_features[:, 0], training_other_features[:, 3])
 pl.show()
+
+"""
+Labels: 0 (Labello), 1 (Not Labello)
+"""
+
+training_features = []
+for i, features in enumerate(training_labello_features):
+    training_features.append([0, features])
+
+for i, features in enumerate(training_other_features):
+    training_features.append([1, features])
+shuffle(training_features)
+features = []
+labels = []
+for f in training_features:
+    labels.append(f[0])
+    features.append(f[1])
+
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit((features), (labels))
+
+true_positive = 0
+true_negative = 0
+false_positive = 0
+false_negative = 0
+for test_features in test_labello_features:
+    prediction = classifier.predict([test_features])
+    print("Prediction: %d, actual: 0" % prediction)
+    if prediction == 0:
+        true_positive += 1
+    else:
+        false_negative += 1
+        
+for test_features in test_other_features:
+    prediction = classifier.predict([test_features])
+    print("Prediction: %d, actual: 1" % prediction)
+    if prediction == 1:
+        true_negative += 1
+    else:
+        false_positive += 1
+        
+
+print("TP %d, TN: %d, FP: %d, FN: %d" % (true_positive, true_negative, false_positive, false_negative))
