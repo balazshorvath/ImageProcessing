0174e7adcc52ab1e1b05ad08cdf45cd6e91d0be8
balazshorvath <balazs.peter.horvath@gmail.com>
2018-05-11 07:53:30 +0200

    training dnn

--- coke_nn/coke_dnn.py
+++ coke_nn/coke_dnn.py
@@ -1,32 +1,45 @@
-from helpers.nn_helper import create_dnn
-from tensorflow.examples.tutorials.mnist import input_data
+from helpers.nn_helper import *
 import tensorflow as tf
 
 
 def load_classification(file_path):
     csv_file = open(file_path, "r")
-    labels = {}
+    _labels = {}
     for row in csv_file:
         r = row.split(",")
-        labels[r[0].strip()] = float(r[1].strip())
-    return labels
+        _labels[r[0].strip()] = float(r[1].strip())
+    return _labels
 
 
 def load_features(file_path):
     csv_file = open(file_path, "r")
-    features = {}
+    _features = {}
     for row in csv_file:
         r = row.split(",")
         parsed_data = []
         for d in r[1:]:
             parsed_data.append(float(d.strip()))
-        features[r[0]] = parsed_data
-    return features
+        _features[r[0]] = parsed_data
+    return _features
 
 
-bottle_features = load_features("data.csv")
-bottle_labels = load_classification("simple_classification.csv")
+features = load_features("data.csv")
+labels = load_classification("simple_classification.csv")
+training_features = []
+training_labels = []
 
-x = tf.placeholder("float", [None, 6])
-y = tf.placeholder("float", [None, 10])
-input_data.read_data_sets("tmp/mnist", one_hot=True).train.next_batch()
+for key, label in features:
+    feature = features.get(key)
+    if feature is None:
+        print("Didn't find features of %s" % key)
+        continue
+    training_features.append(feature)
+    training_labels.append(transform_to_one_hot([label], hot_size=9))
+
+epochs = 10
+
+x = tf.placeholder("float", [None, 8])
+label_placeholder = tf.placeholder("float")
+
+dnn = create_dnn([10, 10, 10], n_features=8, n_classes=9)
+train_dnn(dnn, label_placeholder, DataContainer(training_features, training_labels, 10), 20)
--- helpers/nn_helper.py
+++ helpers/nn_helper.py
@@ -1,7 +1,75 @@
 import tensorflow as tf
+import nu
 
 
-def create_dnn(data, layer_neurons: [], randomize_biases=False, **kwargs):
+class DataContainer:
+    batch_size: int
+    batches = []
+    current_batch: 0
+
+    def __init__(self, features, labels, batch_size=1) -> None:
+        super().__init__()
+        data_length = len(features)
+        if data_length != len(labels):
+            raise ValueError("Features and labels have different lengths!")
+        if batch_size < 1:
+            raise ValueError("Batch size must be greater, than 0!")
+        self.batch_size = batch_size
+        for i in range(len(features / batch_size)):
+            last_index = (i + 1) * batch_size
+            if last_index > data_length:
+                last_index = data_length
+
+            self.batches.append((features[i * batch_size:last_index], labels[i * batch_size:last_index]))
+
+    def next(self):
+        if self.current_batch >= len(self.batches):
+            raise IndexError("No more batches!")
+        batch = self.batches[self.current_batch]
+        self.current_batch += 1
+        return batch
+
+    def has_next(self):
+        if self.current_batch >= len(self.batches):
+            return False
+        return True
+
+    def reset(self):
+        self.current_batch = 0
+
+
+def transform_to_one_hot(labels: [], **kwargs):
+    """
+    Create a list with the size of maximum value inside the parameter list.
+    The one hot will be at the index of a label value.
+    Example:
+
+    For: labels = [1, 2, 4]
+    Result will be: [[0,0,0,1],[0,0,1,0],[1,0,0,0]]
+
+    The label values should be "compressed" (consecutive number only), this way there won't be elements which will
+    never have the value 1.
+
+    There's an optional parameter "hot_size", which sets the width of the result.
+    Useful, if the labels array might not contain all the possible values.
+
+    :param labels: array-like containing integers
+    :return: matrix with the width of the max(labels) and length of len(labels)
+    """
+    if "hot_size" is kwargs:
+        hot_size = kwargs["hot_size"]
+    else:
+        hot_size = max(labels)
+    result = []
+    for label in labels:
+        one_hot = [0] * hot_size
+        one_hot[label] = 1
+        result.append(one_hot)
+
+    return result
+
+
+def create_dnn(layer_neurons: [], randomize_biases=False, **kwargs):
     # Check if the number of classes and features are provided explicitly
     # If so, add them to the list of neurons
     # This might be a stupid feature, but whatever.
@@ -10,7 +78,7 @@ def create_dnn(data, layer_neurons: [], randomize_biases=False, **kwargs):
     if "n_features" in kwargs:
         layer_neurons.insert(0, kwargs["n_features"])
 
-    current_data = data
+    current_data = tf.placeholder("float", [None, len(layer_neurons[0])])
     nn_len = len(layer_neurons)
     for i in range(nn_len - 2):
         if randomize_biases:
@@ -42,3 +110,18 @@ def create_dnn(data, layer_neurons: [], randomize_biases=False, **kwargs):
         ),
         biases
     )
+
+
+def train_dnn(dnn, label_placeholder, train_data: DataContainer, epochs, learning_rate=0.001):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(dnn, label_placeholder))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    with tf.Session() as session:
+        session.run(tf.initialize_all_variables())
+        for epoch in range(epochs):
+            epoch_loss = 0
+            while train_data.has_next():
+                x, y = train_data.next()
+                _, c = session.run([optimizer, cost], feed_dict={x: x, y: y})
+                epoch_loss = c
+            print("Epoch %d/%d completed. Loss for this epoch was %.2f" % (epoch, epochs, epoch_loss))
