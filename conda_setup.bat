6e196bbe2b129a1c365319745a228698e24f1190
balazshorvath <balazs.peter.horvath@gmail.com>
2018-05-24 10:42:46 +0200

    Required libraries for the coke_nn project. Should be extended over time.

---
+++ conda_setup.bat
@@ -0,0 +1,5 @@
+conda create -y -n ImageProcessing python=3.6 anaconda
+conda install -y -n Imageprocessing numpy
+conda install -y -n Imageprocessing matplotlib
+conda install -y -n Imageprocessing -c conda-forge opencv
+conda install -y -n Imageprocessing -c conda-forge tensorflow
